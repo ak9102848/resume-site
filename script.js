8ac586aeebdf2b6c56434308419191482f94898d
ak9102848 <160004831+ak9102848@users.noreply.github.com>
2024-07-21 18:17:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const darkModeToggle = document.getElementById('darkModeToggle');
+    const form = document.forms['submit-to-google-sheet'];
+    const formMessage = document.getElementById('formMessage');
+
+    darkModeToggle.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+    });
+
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+        fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+            .then(response => {
+                console.log('Success!', response);
+                formMessage.style.display = 'block'; // Show success message
+                form.reset(); // Clear form fields
+                setTimeout(() => {
+                    formMessage.style.display = 'none'; // Hide success message after 3 seconds
+                }, 3000);
+            })
+            .catch(error => console.error('Error!', error.message));
+    });
+});
+
